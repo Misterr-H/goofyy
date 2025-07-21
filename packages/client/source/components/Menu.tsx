1b11c2ac75ca0ff6e458262c3a475bed00bf4e4c
Daniel <35579566+wieserd@users.noreply.github.com>
2025-07-21 16:44:43 +0200

    Update Menu.tsx

--- packages/client/source/components/Menu.tsx
+++ packages/client/source/components/Menu.tsx
@@ -13,12 +13,12 @@ interface MenuProps {
 
 export function Menu({ items, selectedIndex }: MenuProps) {
   return (
-    <Box flexDirection="column" marginBottom={1}>
-      <Text bold>Goofyy Menu</Text>
+    <Box flexDirection="row" marginBottom={1}>
+      <Text bold>Goofyy Menu: </Text>
       <Box>
         {items.map((item, index) => (
           <Text key={item.label} color={index === selectedIndex ? "blue" : "white"}>
-            {index === selectedIndex ? '> ' : '  '}{item.label}  
+            {index === selectedIndex ? '> ' : '  '}{item.label}
           </Text>
         ))}
       </Box>
